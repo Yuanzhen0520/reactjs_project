7d752b415006166ab33ac5440177cfe0576b2fdc
Yuanzhen0520 <yzhang18@scu.edu>
2023-08-15 16:26:32 -0700

    fix
    changed home page and add internet plan

--- src/Components/BlazeScreenshot.js
+++ src/Components/BlazeScreenshot.js
@@ -3,77 +3,148 @@ import "./Stylesheet/BlazeScreenshot.css";
 
 const BlazeScreenshot = () => {
   return (
-    <section className="screenshot" id="sshots">
-      <p>
-          Our Phone Plans
-      </p>
-      <div className="phone">
-        <div className="phone_plan">
-          <h1 className="plan_name">5GB/mo.</h1>
-          <p className="text">
-            <ul className="plan_disc">
-              <li className="list">Unlimited talk & text</li>
-              <li className="list">Nationwide coverage</li>
-              <li className="list">Spam protection</li>
-              <li className="list">5GB of 5G or 4G LTE</li>
-            </ul>
-            <button data-btnText="$15" type="button" className="plan_btn">
-              <span>$15</span>
-            </button>
-          </p>
+    <section>
+      <div className="screenshot" id="sshots">
+        <p>Our Phone Plans</p>
+        <div className="phone">
+          <div className="phone_plan">
+            <h1 className="plan_name">5GB/mo.</h1>
+            <p className="text">
+              <ul className="plan_disc">
+                <li className="list">Unlimited talk & text</li>
+                <li className="list">Nationwide coverage</li>
+                <li className="list">Spam protection</li>
+                <li className="list">5GB of 5G or 4G LTE</li>
+              </ul>
+              <button data-btnText="$15" type="button" className="plan_btn">
+                <span>$15</span>
+              </button>
+            </p>
+          </div>
+          <div className="phone_plan">
+            <h1 className="plan_name">10GB/mo.</h1>
+            <p className="text">
+              <ul className="plan_disc">
+                <li className="list">Unlimited talk & text</li>
+                <li className="list">Nationwide coverage</li>
+                <li className="list">Spam protection</li>
+                <li className="list">10GB of 5G or 4G LTE</li>
+                <li className="list">5GB limited hotspot</li>
+              </ul>
+              <button data-btnText="$30" type="button" className="plan_btn">
+                <span>$30</span>
+              </button>
+            </p>
+          </div>
+          <div className="phone_plan">
+            <h1 className="plan_name">15GB/mo.</h1>
+            <p className="text">
+              <ul className="plan_disc">
+                <li className="list">Unlimited talk & text</li>
+                <li className="list">Nationwide coverage</li>
+                <li className="list">Spam protection</li>
+                <li className="list">15GB of 5G or 4G LTE</li>
+                <li className="list">10GB limited hotspot</li>
+              </ul>
+              <button data-btnText="$45" type="button" className="plan_btn">
+                <span>$45</span>
+              </button>
+            </p>
+          </div>
+          <div className="phone_plan">
+            <h1 className="plan_name">Unlimited/mo.</h1>
+            <p className="text">
+              <ul className="plan_disc">
+                <li className="list">Unlimited talk & text</li>
+                <li className="list">Nationwide coverage</li>
+                <li className="list">Spam protection</li>
+                <li className="list">Unlimited of 5G or 4G LTE</li>
+                <li className="list">Unlimited hotspot</li>
+              </ul>
+              <button data-btnText="$60" type="button" className="plan_btn">
+                <span>$60</span>
+              </button>
+            </p>
+          </div>
         </div>
-        <div className="phone_plan">
-          <h1 className="plan_name">10GB/mo.</h1>
-          <p className="text">
-            <ul className="plan_disc">
-              <li className="list">Unlimited talk & text</li>
-              <li className="list">Nationwide coverage</li>
-              <li className="list">Spam protection</li>
-              <li className="list">10GB of 5G or 4G LTE</li>
-              <li className="list">5GB limited hotspot</li>
-            </ul>
-            <button data-btnText="$30" type="button" className="plan_btn">
-              <span>$30</span>
-            </button>
-          </p>
-        </div>
-        <div className="phone_plan">
-          <h1 className="plan_name">15GB/mo.</h1>
-          <p className="text">
-            <ul className="plan_disc">
-              <li className="list">Unlimited talk & text</li>
-              <li className="list">Nationwide coverage</li>
-              <li className="list">Spam protection</li>
-              <li className="list">15GB of 5G or 4G LTE</li>
-              <li className="list">10GB limited hotspot</li>
-            </ul>
-            <button data-btnText="$45" type="button" className="plan_btn">
-              <span>$45</span>
-            </button>
-          </p>
-        </div>
-        <div className="phone_plan">
-          <h1 className="plan_name">Unlimited/mo.</h1>
-          <p className="text">
-            <ul className="plan_disc">
-              <li className="list">Unlimited talk & text</li>
-              <li className="list">Nationwide coverage</li>
-              <li className="list">Spam protection</li>
-              <li className="list">Unlimited of 5G or 4G LTE</li>
-              <li className="list">Unlimited hotspot</li>
-            </ul>
-            <button data-btnText="$60" type="button" className="plan_btn">
-              <span>$60</span>
-            </button>
-          </p>
-        </div>
+        <p className="blazeScreenshot">
+          For customized plans visit{" "}
+          <a href="https://t.me/projectblazeupdates/98" className="sslink">
+            <b>Here</b>
+          </a>
+        </p>
       </div>
-      <p className="blazeScreenshot">
-        For customized plans visit{" "}
-        <a href="https://t.me/projectblazeupdates/98" className="sslink">
-          <b>Here</b>
-        </a>
-      </p>
+
+      <div className="screenshot" id="sshots">
+        <p>Our Internet Plans</p>
+        <div className="phone">
+          <div className="phone_plan">
+            <h1 className="plan_name">5GB/mo.</h1>
+            <p className="text">
+              <ul className="plan_disc">
+                <li className="list">Unlimited talk & text</li>
+                <li className="list">Nationwide coverage</li>
+                <li className="list">Spam protection</li>
+                <li className="list">5GB of 5G or 4G LTE</li>
+              </ul>
+              <button data-btnText="$15" type="button" className="plan_btn">
+                <span>$15</span>
+              </button>
+            </p>
+          </div>
+          <div className="phone_plan">
+            <h1 className="plan_name">10GB/mo.</h1>
+            <p className="text">
+              <ul className="plan_disc">
+                <li className="list">Unlimited talk & text</li>
+                <li className="list">Nationwide coverage</li>
+                <li className="list">Spam protection</li>
+                <li className="list">10GB of 5G or 4G LTE</li>
+                <li className="list">5GB limited hotspot</li>
+              </ul>
+              <button data-btnText="$30" type="button" className="plan_btn">
+                <span>$30</span>
+              </button>
+            </p>
+          </div>
+          <div className="phone_plan">
+            <h1 className="plan_name">15GB/mo.</h1>
+            <p className="text">
+              <ul className="plan_disc">
+                <li className="list">Unlimited talk & text</li>
+                <li className="list">Nationwide coverage</li>
+                <li className="list">Spam protection</li>
+                <li className="list">15GB of 5G or 4G LTE</li>
+                <li className="list">10GB limited hotspot</li>
+              </ul>
+              <button data-btnText="$45" type="button" className="plan_btn">
+                <span>$45</span>
+              </button>
+            </p>
+          </div>
+          <div className="phone_plan">
+            <h1 className="plan_name">Unlimited/mo.</h1>
+            <p className="text">
+              <ul className="plan_disc">
+                <li className="list">Unlimited talk & text</li>
+                <li className="list">Nationwide coverage</li>
+                <li className="list">Spam protection</li>
+                <li className="list">Unlimited of 5G or 4G LTE</li>
+                <li className="list">Unlimited hotspot</li>
+              </ul>
+              <button data-btnText="$60" type="button" className="plan_btn">
+                <span>$60</span>
+              </button>
+            </p>
+          </div>
+        </div>
+        <p className="blazeScreenshot">
+          For customized plans visit{" "}
+          <a href="https://t.me/projectblazeupdates/98" className="sslink">
+            <b>Here</b>
+          </a>
+        </p>
+      </div>
     </section>
   );
 };
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -9,20 +9,33 @@ const Home = () => {
       <div className="father d-flex flex-column flex-md-row container justify-content-between align-items-center my-5">
         <div className="child-1 order-2 order-md-1">
           <h2>
-            Introducing
-            <br />
-            Z-Mobile
+            Welcome to
+            <h3 className="brand">Z Mobile</h3>
           </h2>
-          <h3 className="headerh3">Embrace a new era of seamless connectivity</h3>
+          <h3 className="headerh3">Stay Connected, Your Way!</h3>
           <p>All your needs are satisfied here!</p>
-          <Link to="/download" style={{ textDecoration: "none" }}>
+          <Link to="/screenshot" style={{ textDecoration: "none" }}>
             <button type="button" className="downloadBtn">
-              Shop Now <img src={"images/arrow.png"} alt="arrow" />
+              Shop Now
             </button>
           </Link>
         </div>
         <div className="child-2 text-center my-5 my-md-0 order-md-2 order-1">
-          <img src={"images/device.png"} alt="Device" className="controller" />
+          <img
+            src={"images/smiling_person1.png"}
+            alt="Device"
+            className="controller"
+          />
+          <img
+            src={"images/smiling_person2.png"}
+            alt="Device"
+            className="controller"
+          />
+          <img
+            src={"images/smiling_man.png"}
+            alt="Device"
+            className="controller"
+          />
         </div>
       </div>
     </div>
